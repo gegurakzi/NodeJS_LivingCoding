36b9f51933cbd4a0a05e48d6bfa777e0452f7842
Malachai <xogmldntmd@naver.com>
2022-01-17 21:25:21 +0900

    220117 - 27강 까지

--- main.js
+++ main.js
@@ -2,6 +2,31 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
+function templateHTML(title, list, body){
+    return `<!doctype html>
+    <html>
+    <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+    </head>
+    <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+    </body>
+    </html>
+    `;
+}
+
+function templateLIST(filelist){
+    var list = '<ul>';
+    filelist.forEach(elem => {
+        list = list+'<li><a href="/?id='+elem+'">'+elem+'</a></li>';
+    });
+    list = list+'</ul>';
+    return list;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -14,28 +39,8 @@ var app = http.createServer(function(request,response){
             fs.readdir('./data/', (err, filelist)=>{
                 var title = "Home"
                 var description = "Hello Node.js";
-
-                var list = '<ul>';
-                filelist.forEach(elem => {
-                    list = list+'<li><a href="/?id='+elem+'">'+elem+'</a></li>';
-                });
-                list = list+'</ul>';
-
-                var template = `
-                <!doctype html>
-                <html>
-                <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-                </head>
-                <body>
-                <h1><a href="/">WEB</a></h1>
-                ${list}
-                <h2>${title}</h2>
-                <p>${description}</p>
-                </body>
-                </html>
-                `;
+                var list = templateLIST(filelist);
+                var template =templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
                 response.writeHead(200);
                 response.end(template);
             })
@@ -43,28 +48,8 @@ var app = http.createServer(function(request,response){
             fs.readdir('./data/', (err, filelist)=>{
                 fs.readFile(`data/${queryData.id}`, 'utf8', function(err, data){
                     var description = data;
-
-                    var list = '<ul>';
-                    filelist.forEach(elem => {
-                        list = list+'<li><a href="/?id='+elem+'">'+elem+'</a></li>';
-                    });
-                    list = list+'</ul>';
-
-                    var template = `
-                    <!doctype html>
-                    <html>
-                    <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                    </head>
-                    <body>
-                    <h1><a href="/">WEB</a></h1>
-                    ${list}
-                    <h2>${title}</h2>
-                    <p>${description}</p>
-                    </body>
-                    </html>
-                    `;
+                    var list = templateLIST(filelist);
+                    var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
                     response.writeHead(200);
                     response.end(template);
                 })
